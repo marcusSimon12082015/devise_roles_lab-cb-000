d66316bf306ea15e5fda447c232ff5b755528f41
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-03-03 16:01:50 +0000

    Automatically backed up by Learn

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def destory
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/new'
+  get 'users/destory'
+  get 'users/update'
   devise_for :users
 
   root to: 'users#index'
