80c6f82d3d7c742c4523604b17487b5b64a9ab19
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-03-03 17:47:11 +0000

    Automatically backed up by Learn

--- db/migrate/20190303174507_create_posts.rb
+++ db/migrate/20190303174507_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.belongs_to :user 
+      t.belongs_to :user
       t.string :content
       t.timestamps
     end
