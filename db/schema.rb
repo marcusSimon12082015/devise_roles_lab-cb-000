98a5526f4a97bf7fc1c7d26d8f959c9c5dc39715
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-03-03 16:27:39 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2016_01_18_210027) do
+ActiveRecord::Schema.define(version: 2019_03_03_162522) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2016_01_18_210027) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.integer "role"
+    t.integer "role", default: 0
     t.string "invitation_token"
     t.datetime "invitation_created_at"
     t.datetime "invitation_sent_at"
