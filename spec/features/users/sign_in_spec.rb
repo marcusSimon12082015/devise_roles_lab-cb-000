cde8c3c3d2ece6803b90e02ab59d9dc87f6f8cac
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-03-03 17:07:35 +0000

    Automatically backed up by Learn

--- spec/features/users/sign_in_spec.rb
+++ spec/features/users/sign_in_spec.rb
@@ -19,7 +19,7 @@ feature 'Sign in', :devise do
   #   When I sign in with valid credentials
   #   Then I see a success message
   scenario 'user can sign in with valid credentials' do
-    user = FactoryGirl.create(:user)
+    user = FactoryBot.create(:user)
     sign_in(user.email, user.password)
     expect(page).to have_content I18n.t 'devise.sessions.signed_in'
   end
