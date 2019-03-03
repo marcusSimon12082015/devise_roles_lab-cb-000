73186e97009ded3d9e990b82f817245f4ff30040
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-03-03 15:51:05 +0000

    Automatically backed up by Learn

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     confirmed_at {Time.now}
-    name "Test User"
+    name {"Test User"}
     sequence(:email) { |n| "user#{n}@example.com" }
     password "please123"
 
