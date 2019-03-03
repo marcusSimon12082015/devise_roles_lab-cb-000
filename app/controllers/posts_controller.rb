f2aeaba0fb66e29f106b6ebe2ac20c41299d201c
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-03-03 18:19:45 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   def update
     if current_user.vip? || current_user.admin?
       @post = Post.find(params[:id])
-      @post.update(params[:content])
+      @post.update(posts_params)
     end
   end
 
